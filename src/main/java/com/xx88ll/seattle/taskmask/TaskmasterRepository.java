2caa816fdd8d202ebc86a4dfffca86c4eb69bc1e
xialiu1988 <liuxia198896@gmail.com>
2019-07-02 14:14:27 -0700

    iupdaed

--- src/main/java/com/xx88ll/seattle/taskmask/TaskmasterRepository.java
+++ src/main/java/com/xx88ll/seattle/taskmask/TaskmasterRepository.java
@@ -3,8 +3,6 @@ package com.xx88ll.seattle.taskmask;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 @EnableScan
 public interface TaskmasterRepository extends CrudRepository<Task, String> {
 
