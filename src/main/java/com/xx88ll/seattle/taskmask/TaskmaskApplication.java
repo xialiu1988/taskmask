a28fa9e40478d93cd876d9a9ebe1f78582e46073
xialiu1988 <liuxia198896@gmail.com>
2019-07-02 13:38:27 -0700

    upated

---
+++ src/main/java/com/xx88ll/seattle/taskmask/TaskmaskApplication.java
@@ -0,0 +1,13 @@
+package com.xx88ll.seattle.taskmask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xx88ll/seattle/taskmask/TaskmaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xx88ll.seattle.taskmask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskmaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
